67eaf258f589d0e5ca4dbfa76ed9a5edef2c028b
HyTom <HyyrylainenTommi@gmail.com>
2016-12-27 23:03:53 +0200

    READMEn muokkausta

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-Tommy's Tetris is almost the best tetris in the world.
+*"Tommy's Tetris is almost the best tetris in the world." - Tommi*
+
+Tetris Grand Masteria jäljittelevä java-tetris.
+
+	**Dokumentaatio**
+
+[Aiheen kuvaus ja rakenne](Dokumentaatio/aihemaarittely.md)
+[Tuntikirjanpito](Dokumentaatio/tuntikirjanpito.md)
--- T-Tetris/src/main/java/ttetris/Main.java
+++ T-Tetris/src/main/java/ttetris/Main.java
@@ -5,9 +5,11 @@ import ttetris.logiikka.Tetrispeli;
 public class Main {
 
     public static void main(String[] args) {
+        //Tällä hetkellä pelissä on vain yksi tetris pala jonka voi
+        //lisätä pelikentälle ja liikuttaa alaspäin. Useita samanlaisia
+        //myös lisätä pelikentälle ja liikuttaa alas.
         Tetrispeli peli = new Tetrispeli();
         peli.aloita();
     }
 
 }
-
