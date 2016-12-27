f47892de93a5828c4ec605bd011fd5e9e63000bb
Tommi Hyyryläinen <HyyrylainenTommi@gmail.com>
2016-12-27 23:18:58 +0200

    Update Main.java
    Koodin tilanne

--- T-Tetris/src/main/java/ttetris/Main.java
+++ T-Tetris/src/main/java/ttetris/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         //Tällä hetkellä pelissä on vain yksi tetris pala jonka voi
-        //lisätä pelikentälle ja liikuttaa alaspäin. Useita samanlaisia
+        //lisätä pelikentälle ja liikuttaa alaspäin. Useita samanlaisia voi
         //myös lisätä pelikentälle ja liikuttaa alas.
         Tetrispeli peli = new Tetrispeli();
         peli.aloita();
